3e94277f65aa44b7536d5f963df94803cee10f21
Kathy_Laip <ekaterinaslapnikova@Ekaterinas-MacBook-Air.local>
2023-05-02 18:17:25 +0300

    change port

--- backend/main.py
+++ backend/main.py
@@ -161,4 +161,4 @@ def getOrdersByManager():
     return json.dumps(orders_response)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1")
+    app.run(debug=True, host="127.0.0.1", port="5050")
